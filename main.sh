6baa8474dcd7528165c3f5c157cb5df32d32d9a5
Kela Bari Dos Reis <ab0725@cmpt251.westminstercollege.edu>
2019-11-11 14:21:31 -0700

    Added list_todo function

--- main.sh
+++ main.sh
@@ -1,3 +1,21 @@
 !/bin/bash
 
+#searches for todo items and prints them in order
+function list_todo(){
+	counter=1
+	filenames=$(find ~/todo -name '*')
+	if [ $filenames == "*"]; then
+		for filename in $filenames; do
+			line=$(head -n 1 $filename)
+			echo "$counter) $line"
+			counter=$((counter+1))
+		done
+	else
+		echo "Nothing at all!"
+	fi
+}
+
+echo "Welcome to to-do list manager! Here are your current items."
+list_todo
+echo "What would you like to do?"
 
